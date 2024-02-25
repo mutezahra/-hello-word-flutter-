901951f31053450dbbd815a0acbc479630a0a374
MuthiaZhra <muthiazahra048@gmail.com>
2024-02-25 20:27:04 +0700

    come back

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:js_util';
+
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp( new MaterialApp(
+    home:new HalHallo(),
+  ));
+}
+
+class HalHallo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      body: 
+      new Center(
+        child: new Text("Hallo World"), 
+      
+      )
+    
+      
+
+
+    );
+  }
+}
